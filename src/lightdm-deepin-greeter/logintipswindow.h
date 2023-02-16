630413e0244569164e2bab8cdb20673c35529c94
wangyixue <wangyixue@uniontech.com>
2023-02-16 18:26:42 +0800

    chore: pass reuse check
    Add license and copyright to pass reuse lint
    
    Log: add license and copyright

--- src/lightdm-deepin-greeter/logintipswindow.h
+++ src/lightdm-deepin-greeter/logintipswindow.h
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #ifndef LOGINTIPSWINDOW_H
 #define LOGINTIPSWINDOW_H
 
--- src/widgets/warningview.h
+++ src/widgets/warningview.h
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #ifndef WARNINGVIEW_H
 #define WARNINGVIEW_H
 
